7a50b65e3f356da1a973800c13e7970a80699bed
khush pithva <khushpithva@gmail.com>
2025-12-27 15:37:22 +0530

    Delete Gearguard directory

--- Gearguard/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = 'hackathon-secret-key'
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'instance', 'gearguard.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- Gearguard/models/team.py
+++
@@ -1,6 +0,0 @@
-from .db import db
-
-class Team(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False) # e.g., "IT Support"
-    members = db.relationship('User', backref='team', lazy=True)
